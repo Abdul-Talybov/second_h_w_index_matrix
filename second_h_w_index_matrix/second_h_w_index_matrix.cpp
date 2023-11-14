0b3019f20710913657bc743582e914468014b7ce
Abdul <Abdul@LAPTOP-PI7QTOVL>
2023-11-14 11:38:42 +1000

    Добавьте файлы проекта.

---
+++ second_h_w_index_matrix/second_h_w_index_matrix.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+#include <clocale>
+#include <algorithm>
+
+bool isSymmetric(int* arr, int M) {
+    return std::equal(arr, arr + M / 2, arr + M - 1, std::greater<int>());
+}
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    int N, M;
+    std::cout << "Введите размеры матрицы NxM: ";
+    std::cin >> N >> M;
+
+    std::vector<int*> matrix(N);
+    for (int i = 0; i < N; ++i) {
+        matrix[i] = new int[M];
+        std::cout << "Введите элементы " << i + 1 << "-й строки матрицы: ";
+        for (int j = 0; j < M; ++j) {
+            std::cin >> matrix[i][j];
+        }
+    }
+
+    std::vector<int> B(N);
+    for (int i = 0; i < N; ++i) {
+        B[i] = isSymmetric(matrix[i], M) ? 1 : 0;
+    }
+
+    std::cout << "Массив B: ";
+    for (int i = 0; i < N; ++i) {
+        std::cout << B[i] << ' ';
+    }
+
+    for (int i = 0; i < N; ++i) {
+        delete[] matrix[i];
+    }
+
+    return 0;
+}
